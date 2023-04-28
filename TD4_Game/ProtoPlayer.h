330aaedcc1ab658250c54a4c94ba2ccf0d0b704d
kairin_mup <k020g1208@g.neec.ac.jp>
2023-04-28 17:08:19 +0900

    プレイヤーobj基盤の作成

--- TD4_Game/ProtoPlayer.h
+++ TD4_Game/ProtoPlayer.h
@@ -1,12 +1,44 @@
 #pragma once
 
+#include <vector>
+#include <memory>
+#include <array>
 #include <NY_Object3D.h>
 
 
 
 class ProtoPlayer
 {
+public:
+	ProtoPlayer();
+	~ProtoPlayer();
 
+	void Init();
 
+	void Update();
+
+	void Draw();
+
+	void Finalize();
+
+	//ビンタ
+	void HandAttack();
+
+	//髪を切る
+	void CutHair();
+
+	//左手のアイテムを切り替える
+	void ChangeItem();
+
+private:
+	
+	RVector3 position;
+	RVector3 rotation;
+	RVector3 scale;
+
+	std::shared_ptr<Object3d> player = nullptr;
+	std::shared_ptr<Object3d> scissor = nullptr;
+
+	UINT modelPlayer;
 };
 
