6ba4fbd84f2a29e3691fad3d993418f5edb0306f
kairin_mup <k020g1208@g.neec.ac.jp>
2023-05-12 12:30:32 +0900

    titleSceneのオブジェクトに動きをつけた

--- TD4_Game/TitleScene.cpp
+++ TD4_Game/TitleScene.cpp
@@ -4,10 +4,13 @@
 TitleScene::TitleScene(ISceneChanger *changer) : BaseScene(changer) {
 	UINT testTex = TexManager::LoadTexture("Resources/asp3.png");
 
+
 	testobject = std::make_shared<Object3d>();
 	testobject.reset(NY_Object3DManager::Get()->CreateModel_Box(50.f, 10.f, 10.f, testTex));
 
-	testobject->SetAffineParam(RVector3(1, 1, 1), RVector3(0, 45, 0), RVector3(0, 0, 0));
+	testObjPos = { 0,0,0 };
+
+	testobject->SetAffineParam(RVector3(1, 1, 1), RVector3(0, 45, 0), testObjPos);
 
 	testsp.Create(testTex);
 }
@@ -31,7 +34,7 @@ void TitleScene::Finalize()
 
 //XV
 void TitleScene::Update() {
-
+	Animation();
 	if (Input::Get()->isKeyTrigger(DIK_2)) {
 		mSceneChanger->ChangeScene(eScene_Game);
 	}
@@ -50,4 +53,28 @@ void TitleScene::Draw2D()
 
 void TitleScene::DrawImgui()
 {
+}
+
+void TitleScene::Animation()
+{
+	if (animationCount < animationCountMax)
+	{
+		animationCount++;
+	}
+	else
+	{
+	}
+	if (animationCount < animationCountMax / 4)
+	{
+		testObjPos.x += 0.5f;
+	}
+	else if (animationCount < (animationCountMax / 4 * 3))
+	{
+		testObjPos.x -= 0.5f;
+	}
+	else if (animationCount < animationCountMax)
+	{
+		testObjPos.x += 0.5f;
+	}
+	testobject->SetAffineParam(RVector3(1, 1, 1), RVector3(0, 45, 0), testObjPos);
 }
