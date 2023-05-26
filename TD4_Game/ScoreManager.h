b68fb26a99619768edcee848b0d1975ae2eb32d4
fuuma1223 <k020g1170@g.neec.ac.jp>
2023-05-26 16:30:08 +0900

    実装したこと ・ゲーム中のスコア表示 ・ゲーム中の捌いた人数表示 ・ゲーム中のレビュー表示 ・一部仮リソース追加

--- TD4_Game/ScoreManager.h
+++ TD4_Game/ScoreManager.h
@@ -12,17 +12,21 @@ private:
 	//最大レビュー
 	static const int MAX_REVIEW = 5;
 	//はげの基本レビュー
-	static const int DEFO_HAGEHEAD_REVIEW = 3;
+	static const int DEFO_HAGEHEAD_REVIEW = 2;
 	//一本はげの基本レビュー
-	static const int DEFO_LIGHTHEAD_REVIEW = 3;
+	static const int DEFO_LIGHTHEAD_REVIEW = 2;
 	//アフロの基本レビュー
-	static const int DEFO_AFROHEAD_REVIEW = 3;
-	//いい選択（ゲーム的には悪い選択）(基本レビューに足す値)
+	static const int DEFO_AFROHEAD_REVIEW = 2;
+	//いい選択(ゲーム的には悪い選択) (基本レビューに足す値)
 	static const int GOOD_ACTION = 1;
-	//悪い選択(ゲーム的には良い選択)（基本レビューから引く値）
-	static const int BAD_ACTION = 2;
+	//悪い選択(ゲーム的には良い選択) (基本レビューに足す値)
+	static const int BAD_ACTION = -2;
 	//基本のスコア
-	static const int DEFO_SCORE = 800;
+	static const int DEFO_SCORE = 200;
+	//総合レビューの位置
+	const XMFLOAT2 TOTALREVIEW_POS = { 32,64 };
+	//捌いた数の位置
+	const XMFLOAT2 HANDLE_POS = { 32,0 };
 private:
 	//スプライト
 	Sprite spriteScore;
@@ -32,12 +36,16 @@ private:
 	static float totalScore;
 	//総合的なレビュー
 	static float totalReview;
+	//さばいた数
+	static int handleNum;
 
 	//一時保存用のスコア
-	std::vector<float> scoreNum;
+	float scoreNum;
 
 	//一時保存用のレビュー
 	float oldReview;
+	//１人のレビュー
+	float oneReview;
 	//増減させる為のレビュー
 	float reduceReview;
 	//レビューの計算用
@@ -59,6 +67,14 @@ private:
 	bool scorePrintFlag;
 	//スコアの位置
 	XMFLOAT2 scorePos;
+	//スコアのアルファ値
+	float scoreA;
+	//レビューの位置
+	XMFLOAT2 reviewPos;
+	//レビューがたされる表示のためのフラグ
+	bool reviewPrintFlag;
+	//レビューのアルファ値
+	float reviewA;
 private:
 	//最初のNumセット
 	void FirstNumSet();
@@ -73,5 +89,7 @@ public:
 	static const float &GetScore() { return totalScore; }
 	//レビューの参照
 	static const float &GetReview() { return totalReview; }
+	//捌いた数の参照
+	static const int &GetHandle() { return handleNum; }
 };
 
